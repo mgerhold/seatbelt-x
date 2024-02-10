eb6e6778f17dc76e16afe740cc386947324d6153
Michael Gerhold <michael.gerhold@web.de>
2024-02-10 22:15:22 +0100

    Refactor and expand interpreter tests
    The `InterpreterTests` class has been refactored for better code reusability by extracting the common logic into a separate method `interpret_source`. Further, several more assertions have been added to enhance the `Print` and `PrintLn` test cases ensuring more comprehensive testing of the interpreter's functionality.

--- test/test_interpreter.cpp
+++ test/test_interpreter.cpp
@@ -16,11 +16,27 @@ static void write_text_file(std::filesystem::path const& path, std::string_view 
     assert(file);
 }
 
-TEST(InterpreterTests, Print) {
-    static constexpr auto source = R"(println("test");
-)"sv;
+static std::string interpret_source(std::string_view const source) {
     write_text_file(source_filename, source);
     auto std_out = std::ostringstream{};
     interpret(source_filename, std_out);
-    EXPECT_EQ("test\n"sv, std_out.str());
+    return std::move(std_out).str();
+}
+
+TEST(InterpreterTests, Print) {
+    EXPECT_EQ("test"sv, interpret_source("print(\"test\");\n"sv));
+    EXPECT_EQ("\""sv, interpret_source("print(\"\\\"\");\n"sv));
+    EXPECT_EQ("🦀"sv, interpret_source("print(\"🦀\");\n"sv));
+    EXPECT_EQ("Hello,\nworld!"sv, interpret_source("print(\"Hello,\\nworld!\");\n"sv));
+    EXPECT_EQ("42"sv, interpret_source("print(42);\n"sv));
+    EXPECT_EQ(" "sv, interpret_source("print(\" \");\n"sv));
+}
+
+TEST(InterpreterTests, PrintLn) {
+    EXPECT_EQ("test\n"sv, interpret_source("println(\"test\");\n"sv));
+    EXPECT_EQ("\"\n"sv, interpret_source("println(\"\\\"\");\n"sv));
+    EXPECT_EQ("🦀\n"sv, interpret_source("println(\"🦀\");\n"sv));
+    EXPECT_EQ("Hello,\nworld!\n"sv, interpret_source("println(\"Hello,\\nworld!\");\n"sv));
+    EXPECT_EQ("42\n"sv, interpret_source("println(42);\n"sv));
+    EXPECT_EQ(" \n"sv, interpret_source("println(\" \");\n"sv));
 }
